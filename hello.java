ab056924d3a5473c2e9767b751150565f856e7ec
aslin-123 <119990545+aslin-123@users.noreply.github.com>
2026-03-02 14:17:04 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,8 @@ class example{
 public static void main(String[] args){
 System.out.println("Hello world");
   System.out.println("Hello John");
-System.out.println("Addition: "+(2+3));
+System.out.println("Addition: "+(20+3));
+  System.out.println("Hello John abraham");
 }
 
 }
